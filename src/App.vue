b057d7b0b97e9eb72062097a66c66b35eb822d0c
0xx32 <dimas1523@mail.ru>
2025-01-14 23:03:09 +0300

    init branch, add task management functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
+
+import { Input } from '@/components/ui/input'
 import TodoItem from './components/TodoItem.vue'
+import { Button } from '@/components/ui/button'
+import { Plus, X } from 'lucide-vue-next'
 
 interface Task {
 	id: number
@@ -21,6 +25,23 @@ const addTask = () => {
 
 	localStorage.setItem('tasks', JSON.stringify(tasks.value))
 }
+const clearTasks = () => {
+	tasks.value = []
+	localStorage.removeItem('tasks')
+}
+const removeTask = (id: number) => {
+	tasks.value = tasks.value.filter((task) => task.id !== id)
+	localStorage.setItem('tasks', JSON.stringify(tasks.value))
+}
+const changeIsCompleted = (id: number) => {
+	tasks.value = tasks.value.map((task) => {
+		if (task.id === id) {
+			task.isCompleted = !task.isCompleted
+		}
+		return task
+	})
+	localStorage.setItem('tasks', JSON.stringify(tasks.value))
+}
 
 onMounted(() => {
 	const tasksArr = JSON.parse(localStorage.getItem('tasks') || '[]') as Task[]
@@ -30,19 +51,32 @@ onMounted(() => {
 
 <template>
 	<div class="bg-slate-900 h-screen">
-		<div class="w-1/2 border-white/30 mx-auto pt-40">
-			<form @submit="addTask" @submit.prevent>
-				<div class="mb-4">
-					<input
-						type="text"
-						class="border border-white/50 p-2 w-full bg-transparent rounded-md text-white"
-						v-model="taskInput"
-					/>
+		<div class="w-1/3 border-white/30 mx-auto pt-40">
+			<div class="flex mb-6">
+				<Button class="ml-auto" variant="destructive" @click="clearTasks"> Очистить лист</Button>
+			</div>
+			<form @submit="addTask" @submit.prevent class="mb-6">
+				<div class="mb-4 flex gap-4 items-center">
+					<div class="relative w-full">
+						<Input type="text" v-model="taskInput" placeholder="Введите задачу" />
+						<button type="button" @click="taskInput = ''" class="absolute right-2 top-1/2 -translate-y-1/2">
+							<X class="text-gray-500" />
+						</button>
+					</div>
+
+					<Button type="submit" class="ml-auto"><Plus /> Добавить</Button>
 				</div>
 			</form>
-
-			<div>
-				<TodoItem v-for="task in tasks" :id="task.id" :title="task.title" :isCompleted="task.isCompleted" />
+			<div class="mb-4">
+				<TodoItem
+					v-for="(task, index) in tasks"
+					@removeTask="removeTask"
+					@changeIsCompleted="changeIsCompleted"
+					:id="task.id"
+					:title="task.title"
+					:is-completed="task.isCompleted"
+					:index="index"
+				/>
 			</div>
 		</div>
 	</div>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,17 +1,44 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+import { Trash2, SquareCheckBig } from 'lucide-vue-next'
+import { Button } from '@/components/ui/button'
 interface Props {
 	id: number
 	title: string
 	isCompleted: boolean
+	index: number
 }
 defineProps<Props>()
+const emit = defineEmits(['removeTask', 'changeIsCompleted'])
+
+const isHovered = ref(false)
 </script>
 
 <template>
-	<div class="flex items-center gap-2">
-		<div class="text-gray-500">{{ id }}</div>
+	<div
+		class="flex items-center gap-2 mb-4 pb-2 border-b border-white/30 h-10"
+		@mouseenter="isHovered = true"
+		@mouseleave="isHovered = false"
+	>
+		<div class="text-gray-500">{{ index + 1 }}</div>
 		<div class="text-white">{{ title }}</div>
-		<div v-if="isCompleted" class="text-green-500">Completed</div>
-		<div v-else class="text-red-500">Not completed</div>
+		<div class="ml-auto flex gap-5 items-center">
+			<div class="text-green-500">
+				<button @click="emit('changeIsCompleted', id)">
+					<SquareCheckBig :class="{ 'text-gray-500': isCompleted }" />
+				</button>
+			</div>
+			<Button
+				:class="{
+					'opacity-0': !isHovered,
+					invisible: !isHovered,
+				}"
+				variant="destructive"
+				@click="emit('removeTask', id)"
+			>
+				<Trash2 />
+			</Button>
+		</div>
 	</div>
 </template>
